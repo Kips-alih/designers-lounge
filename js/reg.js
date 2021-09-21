32f3f7f53ae29ba4e41f74b5e1da372a5c66ef97
RobbinGIT <robbin.githimbo@student.moringaschool.com>
2021-09-21 16:18:21 +0300

    add toggling effects

--- js/reg.js
+++ js/reg.js
@@ -4,13 +4,13 @@ var myLog = document.getElementById("login");
 var mySign = document.getElementById("register");
 var logSign = document.getElementById("btn");
 
-function register (){
+function register(){
     myLog.style.left = "-400px";
     mySign.style.left = "50px";
     logSign.style.left = "110px";
 }
-function register (){
-    myLog.style.left = "-400px";
-    mySign.style.left = "50px";
-    logSign.style.left = "110px";
+function login(){
+    myLog.style.left = "50px";
+    mySign.style.left = "450px";
+    logSign.style.left = "0px";
 }
