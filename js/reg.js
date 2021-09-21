2c40ef05653a743eade8fbc01a651e8621829a27
RobbinGIT <robbin.githimbo@student.moringaschool.com>
2021-09-21 16:10:20 +0300

    Add changes html css and js file

---
+++ js/reg.js
@@ -0,0 +1,16 @@
+// toggle buttons 
+
+var myLog = document.getElementById("login");
+var mySign = document.getElementById("register");
+var logSign = document.getElementById("btn");
+
+function register (){
+    myLog.style.left = "-400px";
+    mySign.style.left = "50px";
+    logSign.style.left = "110px";
+}
+function register (){
+    myLog.style.left = "-400px";
+    mySign.style.left = "50px";
+    logSign.style.left = "110px";
+}
